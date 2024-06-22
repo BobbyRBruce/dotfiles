f32d698e0437a8c223cace344aae8576bdcfd5dd
Bobby R. Bruce <bbruce@ucdavis.edu>
2024-06-21 17:03:19 -0700

    Breakup >79 line in brew-install.sh

--- brew-install.sh
+++ brew-install.sh
@@ -9,4 +9,5 @@ then
     exit 0
 fi
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL \
+    https://raw.githubusercontent.com/Homebrew/install/master/install)"
