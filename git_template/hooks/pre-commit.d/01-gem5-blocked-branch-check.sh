2db313dc2d06e71fbd37aea97cded1d13f51c3f5
Bobby R. Bruce <bbruce@ucdavis.edu>
2024-06-30 18:54:57 -0700

    Add .git_template

---
+++ git_template/hooks/pre-commit.d/01-gem5-blocked-branch-check.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+
+# Copyright (c) 2024 Bobby R. Bruce
+# SPDX-License-Identifier: MIT
+#
+# A pre-commit script to ensure I don't commit to my gem5 fork's 'develop' or
+# 'stable' branches. The gem5 project does not allow direct commits to these
+# branches (PRs only), and github's fork sync feature breaks if I update
+# these branches.
+
+rev_parse=$(git rev-parse --abbrev-ref HEAD@{upstream})
+remote=$(echo $rev_parse | cut -d/ -f2)
+branch=$(echo $rev_parse | cut -d/ -f3)
+remote_url=$(git remote --get-url $remote |  rev | cut -d/ -f 1-2 | rev)
+
+
+if [ "$remote_url" = "bobbyrbruce/gem5" ] || \
+[ "$remote_url" = "bobbyrbruce/gem5.git" ]; then
+
+
+if [ "$branch" = "stable" ] || [ "$branch" = "develop" ]; then
+        >&2 echo "You can't commit directly to $branch branch"
+        >&2 echo "Remote: $remote"
+        >&2 echo "Remote URL: $remote_url"
+        exit 1
+    fi
+fi
